29838c5372cacfbef20326e5c3c5dbb09a6da315
Yaroslav Vartsev <futbik1990@gmail.com>
2025-11-24 13:43:21 +0200

    add_folder_vscode

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 $(function(){
-  var mixer = mixitup('.portfolio__content-img');
+
+  $(".menu a, .footer a, .logo").on("click", function (e) {
+		//отменяем стандартную обработку нажатия по ссылке
+		e.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+  
     
   $('.slider-blog__inner').slick({
     dots: true, 
     arrows: false,
     
   });
+
+  $('.menu__button, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+
+  var mixer = mixitup('.portfolio__content-img');
+
+
+
 });
